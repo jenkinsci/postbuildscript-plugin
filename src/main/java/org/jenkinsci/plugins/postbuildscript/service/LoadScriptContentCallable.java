60ed672962b18fa06da2fc32a3ef86906ae0bd3d
Daniel Heid <dheid@posteo.de>
2019-05-19 18:34:15 +0200

    JENKINS-57545 Do not use deprecated method but still apply system charset

--- src/main/java/org/jenkinsci/plugins/postbuildscript/service/LoadScriptContentCallable.java
+++ src/main/java/org/jenkinsci/plugins/postbuildscript/service/LoadScriptContentCallable.java
@@ -6,12 +6,14 @@ import jenkins.MasterToSlaveFileCallable;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 public class LoadScriptContentCallable extends MasterToSlaveFileCallable<String> {
     private static final long serialVersionUID = -8106062333861360484L;
 
     @Override
     public String invoke(File f, VirtualChannel channel) throws IOException {
-        return Util.loadFile(f);
+        return Util.loadFile(f, Charset.defaultCharset());
     }
+
 }
